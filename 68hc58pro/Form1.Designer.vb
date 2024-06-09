a4278fbe6773f8017fea8d7afe770d1fb35f5b87
garne <garne@DESKTOP-VJQFHIA>
2024-06-08 23:39:53 -0400

    Add project files.

---
+++ 68hc58pro/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnConnect = New Button()
+        btnRead = New Button()
+        txtOutput = New TextBox()
+        disconnectBTN = New Button()
+        cmbPorts = New ComboBox()
+        SuspendLayout()
+        ' 
+        ' btnConnect
+        ' 
+        btnConnect.Location = New Point(31, 12)
+        btnConnect.Name = "btnConnect"
+        btnConnect.Size = New Size(90, 23)
+        btnConnect.TabIndex = 0
+        btnConnect.Text = "Connect"
+        btnConnect.UseVisualStyleBackColor = True
+        ' 
+        ' btnRead
+        ' 
+        btnRead.Location = New Point(31, 357)
+        btnRead.Name = "btnRead"
+        btnRead.Size = New Size(90, 23)
+        btnRead.TabIndex = 1
+        btnRead.Text = "RX Mode"
+        btnRead.UseVisualStyleBackColor = True
+        ' 
+        ' txtOutput
+        ' 
+        txtOutput.Location = New Point(127, 13)
+        txtOutput.Multiline = True
+        txtOutput.Name = "txtOutput"
+        txtOutput.ScrollBars = ScrollBars.Both
+        txtOutput.Size = New Size(661, 367)
+        txtOutput.TabIndex = 2
+        ' 
+        ' disconnectBTN
+        ' 
+        disconnectBTN.Location = New Point(31, 41)
+        disconnectBTN.Name = "disconnectBTN"
+        disconnectBTN.Size = New Size(90, 23)
+        disconnectBTN.TabIndex = 3
+        disconnectBTN.Text = "Disconnect"
+        disconnectBTN.UseVisualStyleBackColor = True
+        ' 
+        ' cmbPorts
+        ' 
+        cmbPorts.FormattingEnabled = True
+        cmbPorts.Location = New Point(12, 70)
+        cmbPorts.Name = "cmbPorts"
+        cmbPorts.Size = New Size(109, 23)
+        cmbPorts.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(cmbPorts)
+        Controls.Add(disconnectBTN)
+        Controls.Add(txtOutput)
+        Controls.Add(btnRead)
+        Controls.Add(btnConnect)
+        Name = "Form1"
+        Text = "J1850VPW"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents btnRead As Button
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents disconnectBTN As Button
+    Friend WithEvents cmbPorts As ComboBox
+
+End Class
---
+++ 68hc58pro/Form1.vb
@@ -0,0 +1,133 @@
+﻿Imports System.IO.Ports
+Imports System.Text
+
+Public Class Form1
+
+    Private WithEvents serialPort As New SerialPort()
+    Private frameBuffer As New List(Of String) ' Buffer to store frame bytes
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Configure the serial port
+        serialPort.BaudRate = 115200
+        serialPort.Parity = Parity.None
+        serialPort.StopBits = StopBits.One
+        serialPort.DataBits = 8
+        serialPort.Handshake = Handshake.None
+        serialPort.NewLine = vbLf ' Use Line Feed as the newline character
+        serialPort.DtrEnable = True ' Enable the DTR pin
+
+        ' Disable the Read button initially
+        btnRead.Enabled = False
+        disconnectBTN.Enabled = False
+
+        ' Populate COM ports in the combo box
+        RefreshComPorts()
+    End Sub
+
+    Private Sub RefreshComPorts()
+        cmbPorts.Items.Clear()
+        For Each port As String In SerialPort.GetPortNames()
+            cmbPorts.Items.Add(port)
+        Next
+        If cmbPorts.Items.Count > 0 Then
+            cmbPorts.SelectedIndex = 0
+        End If
+    End Sub
+
+    Private Sub cmbPorts_Click(sender As Object, e As EventArgs) Handles cmbPorts.Click
+        ' Refresh COM ports each time the combo box is clicked
+        RefreshComPorts()
+    End Sub
+
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+        ' Open the serial port
+        If Not serialPort.IsOpen Then
+            Try
+                If cmbPorts.SelectedItem IsNot Nothing Then
+                    serialPort.PortName = cmbPorts.SelectedItem.ToString()
+                    serialPort.Open()
+                    txtOutput.AppendText("Connected to " & serialPort.PortName & Environment.NewLine)
+
+                    ' Send the IDN command
+                    serialPort.WriteLine("idn")
+                    disconnectBTN.Enabled = True
+                    btnConnect.Enabled = False
+                Else
+                    txtOutput.AppendText("Please select a COM port" & Environment.NewLine)
+                End If
+            Catch ex As Exception
+                txtOutput.AppendText("Error: " & ex.Message & Environment.NewLine)
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        ' Send the RX command
+        If serialPort.IsOpen Then
+            serialPort.WriteLine("rx")
+            txtOutput.AppendText("Device set to RX mode" & Environment.NewLine)
+        End If
+    End Sub
+
+    Private Sub serialPort_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles serialPort.DataReceived
+        ' Handle data received from the serial port
+        Dim data As String = serialPort.ReadLine()
+
+        ' Use Invoke to update the UI from the serial port thread
+        Me.Invoke(Sub()
+                      ProcessReceivedData(data)
+                  End Sub)
+    End Sub
+
+    Private Sub ProcessReceivedData(data As String)
+        txtOutput.AppendText("Received: " & data & Environment.NewLine)
+
+        ' Check if the data starts with "RX:"
+        If data.StartsWith("RX:") Then
+            ' Remove "RX:" and split the received data into status byte and data byte
+            Dim parts As String() = data.Substring(3).Trim().Split(" "c)
+            If parts.Length >= 2 Then
+                Dim statusByte As String = parts(0)
+                Dim dataByte As String = parts(1)
+
+                ' Add the data byte to the frame buffer
+                frameBuffer.Add(dataByte)
+
+                ' Check if the status byte indicates the end of the frame
+                If statusByte = "D0" OrElse statusByte = "F0" Then
+                    ' Join the frame buffer into a single string
+                    Dim frame As String = String.Join(" ", frameBuffer)
+                    txtOutput.AppendText("Complete Frame: " & frame & Environment.NewLine)
+
+                    ' Clear the frame buffer for the next frame
+                    frameBuffer.Clear()
+                End If
+            End If
+        Else
+            ' Handle other responses, e.g., IDN response
+            If data.Contains("68hc58-PRO") Then
+                txtOutput.AppendText("Device identified successfully." & Environment.NewLine)
+                btnRead.Enabled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        ' Close the serial port when the form is closing
+        If serialPort.IsOpen Then
+            serialPort.WriteLine("standby")
+            serialPort.Close()
+        End If
+    End Sub
+
+    Private Sub disconnectBTN_Click(sender As Object, e As EventArgs) Handles disconnectBTN.Click
+        If serialPort.IsOpen Then
+            serialPort.WriteLine("standby")
+            serialPort.Close()
+            btnRead.Enabled = False
+        End If
+        disconnectBTN.Enabled = False
+        btnConnect.Enabled = True
+    End Sub
+
+End Class
